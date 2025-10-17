11f75bc9a51d602f6e2bf265fab24c1dd2569122
mhrofficialid <kurniawatihartika24@gmail.com>
2025-10-17 21:37:29 +0700

    Add files via upload

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,1 +1,12 @@
 
+import React from 'react';
+
+const Footer: React.FC = () => (
+    <footer className="text-center mt-12 text-xs text-gray-500">
+        <p><strong>Lisensi Generator Karakter Branding by MHR Studio: "Personal Use".</strong></p>
+        <p className="mt-1">Dilarang keras menyebarluaskan/menjual ulang/menyalin generator ini dengan alasan apapun.</p>
+        <p className="mt-1">Jika ingin ikut menjual produk ini, bisa melalui Private Affiliate hubungi IG/Threads @mhrofficial.id</p>
+    </footer>
+);
+
+export default Footer;
